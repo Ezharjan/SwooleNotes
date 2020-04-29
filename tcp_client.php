1e068b75c67dd606351e6d7d428bcc6f62f7bc46
Alexander <ezharjan@aliyun.com>
2020-04-29 22:18:59 +0800

    added TCP client

---
+++ tcp_client.php
@@ -0,0 +1,19 @@
+<?php
+
+//连接Swoole TCP服务
+$client = new swoole_client(SWOOLE_SOCK_TCP);
+
+if (!$client->connect("127.0.0.1",9501)) {
+    echo "connection failed!";
+    exit;
+}
+
+fwrite(STDOUT, "Please enter your message:");
+$message = trim(fgets(STDIN));
+
+//发送消息给TCP服务器
+client->send($message);
+
+//接收来自服务端的消息
+$result = $client->recv();
+echo $result;
